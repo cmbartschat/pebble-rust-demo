7d453c3bda0e2c55533d5a7337a7033e59954aea
Christoph Bartschat <christoph@barts.chat>
2026-06-23 19:01:59 -0700

    simple menu

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,33 @@
-use pebble_rust_2026::{APP, log_c_str};
+use alloc::boxed::Box;
+use pebble_rust_2026::{
+    APP, SimpleMenuItem, SimpleMenuLayer, SimpleMenuSection, Window, log_c_str,
+};
 
 use crate::{flash::flash, move_box::move_box};
 
 pub fn run_app() {
-    {
-        APP.show(flash());
-    }
+    let mut menu_window = Window::new().unwrap();
 
-    {
+    let mut options = SimpleMenuSection::new_untitled();
+    options.push(SimpleMenuItem::new("Flash", None, None, move || {
+        log_c_str(c"selected option 1");
+        APP.show(flash());
+    }));
+
+    options.push(SimpleMenuItem::new("Move Box", None, None, move || {
+        log_c_str(c"selected option 2");
         APP.show(move_box());
-    }
+    }));
+
+    let mut menu = SimpleMenuLayer::new(
+        menu_window.get_bounds(),
+        &mut menu_window,
+        Box::new([options]),
+    )
+    .unwrap();
+    menu_window.add_child(&mut menu);
+
+    APP.show(menu_window);
 
     APP.event_loop();
 
