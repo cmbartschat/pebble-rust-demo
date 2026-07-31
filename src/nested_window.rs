f6247089f3c21eafa4483f23122e63c8f5119c38
Christoph Bartschat <christoph@barts.chat>
2026-07-31 08:42:41 -0700

    Save

--- src/nested_window.rs
+++ src/nested_window.rs
@@ -1,7 +1,5 @@
 use alloc::boxed::Box;
-use pebble_rust_2026::{
-    APP, Button, GColor, TextLayer, Window, fmt, hex_color, log_c_str, log_fmt,
-};
+use pebble_rust_2026::{APP, Button, GColor, TextLayer, Window, color, fmt, hex_color, log_c_str};
 
 static COLORS: [GColor; 3] = [hex_color!("#ff0"), hex_color!("#f0f"), hex_color!("#0ff")];
 
@@ -12,12 +10,11 @@ pub fn nested_window() -> Window {
 fn _nested_window(i: usize) -> Window {
     let mut window = Window::new().unwrap();
     let bounds = window.get_bounds();
+    window.set_background_color(color::GCOLOR_BLACK);
 
     let fill = {
         let window = window.downgrade();
         move || {
-            log_c_str(c"fill");
-
             let Some(mut window) = window.upgrade() else {
                 log_c_str(c"fill has no window to use");
                 return;
@@ -26,11 +23,8 @@ fn _nested_window(i: usize) -> Window {
             let mut text_layer = TextLayer::new(bounds).unwrap();
             text_layer.set_background_color(COLORS[i % COLORS.len()]);
             text_layer.set_text(&unsafe { fmt!(c"Level %u", i).unwrap() });
-            // return log_c_str(c"early out");
 
-            log_c_str(c"add child");
             window.add_child(&mut text_layer);
-            log_c_str(c"add child done.");
         }
     };
 
@@ -40,21 +34,17 @@ fn _nested_window(i: usize) -> Window {
             let Some(mut window) = window.upgrade() else {
                 return;
             };
-            log_c_str(c"remove_child_layers");
+            window.set_background_color(color::GCOLOR_BLACK);
             window.remove_child_layers();
-            log_c_str(c"remove_child_layers done.");
         }
     };
 
     window.set_appear_effect(Box::new(move || {
-        unsafe { log_fmt!(c"Level %u appeared", i) };
-
         fill();
 
         let clear = clear.clone();
 
         Box::new(move || {
-            unsafe { log_fmt!(c"Level %u disappeared", i) };
             clear();
         })
     }));
