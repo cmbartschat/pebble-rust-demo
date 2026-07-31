9297062251c5a694d1a7744cb8a48c0c659f3de4
Christoph Bartschat <christoph@barts.chat>
2026-07-31 08:43:00 -0700

    remount nested content dynamically

--- src/nested_window.rs
+++ src/nested_window.rs
@@ -1,4 +1,5 @@
-use pebble_rust_2026::{APP, Button, GColor, TextLayer, Window, fmt, hex_color};
+use alloc::boxed::Box;
+use pebble_rust_2026::{APP, Button, GColor, TextLayer, Window, color, fmt, hex_color, log_c_str};
 
 static COLORS: [GColor; 3] = [hex_color!("#ff0"), hex_color!("#f0f"), hex_color!("#0ff")];
 
@@ -8,11 +9,45 @@ pub fn nested_window() -> Window {
 
 fn _nested_window(i: usize) -> Window {
     let mut window = Window::new().unwrap();
+    let bounds = window.get_bounds();
+    window.set_background_color(color::GCOLOR_BLACK);
 
-    let mut text_layer = TextLayer::new(window.get_bounds()).unwrap();
-    text_layer.set_background_color(COLORS[i % COLORS.len()]);
-    text_layer.set_text(&unsafe { fmt!(c"Level %u", i).unwrap() });
-    window.add_child(&mut text_layer);
+    let fill = {
+        let window = window.downgrade();
+        move || {
+            let Some(mut window) = window.upgrade() else {
+                log_c_str(c"fill has no window to use");
+                return;
+            };
+
+            let mut text_layer = TextLayer::new(bounds).unwrap();
+            text_layer.set_background_color(COLORS[i % COLORS.len()]);
+            text_layer.set_text(&unsafe { fmt!(c"Level %u", i).unwrap() });
+
+            window.add_child(&mut text_layer);
+        }
+    };
+
+    let clear = {
+        let window = window.downgrade();
+        move || {
+            let Some(mut window) = window.upgrade() else {
+                return;
+            };
+            window.set_background_color(color::GCOLOR_BLACK);
+            window.remove_child_layers();
+        }
+    };
+
+    window.set_appear_effect(Box::new(move || {
+        fill();
+
+        let clear = clear.clone();
+
+        Box::new(move || {
+            clear();
+        })
+    }));
 
     window.set_click_provider(move |b| {
         b.single(
